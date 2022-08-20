6a3d99a0645494d63c203d7f8c65a061a96dac1f
Allan Selvan <allantony2008@gmail.com>
2022-08-20 10:44:55 +0200

    added docs

--- sync.go
+++ sync.go
@@ -7,15 +7,21 @@ const (
 )
 
 type (
+	// SyncManager describes the interface that can be used to perform sync operations on Anki.
 	SyncManager interface {
 		Trigger() *errors.RestErr
 	}
 
+	// syncManager implements SyncManager
 	syncManager struct{
 		Client *Client
 	}
 )
 
+// Trigger syncs local Anki data to Anki web.
+// The method returns an error if:
+//	- the api request to ankiconnect fails.
+//	- the api returns a http error.
 func (sm *syncManager) Trigger() *errors.RestErr {
 	_, restErr := post[string, ParamsDefault](sm.Client, ActionSync, nil)
 	if restErr != nil {
