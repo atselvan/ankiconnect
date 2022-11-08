c59528b5b88c6e6e8acc5da6cd82a56f6193c137
David Langevin <dlangevi@uwaterloo.ca>
2022-11-08 12:36:21 -0800

    Implement FindNotes api

--- notes.go
+++ notes.go
@@ -14,6 +14,7 @@ type (
 	// Notes manager describes the interface that can be used to perform operation on the notes in a deck.
 	NotesManager interface {
 		Add(note Note) *errors.RestErr
+		Get(query string) (*[]ResultNotesInfo, *errors.RestErr)
 	}
 
 	// notesManager implements NotesManager.
@@ -24,6 +25,24 @@ type (
 	// ParamsCreateNote represents the ankiconnect API params for creating a note.
 	ParamsCreateNote struct {
 		Note *Note `json:"note,omitempty"`
+	}
+
+	// ParamsGetNotes represents the ankiconnect API params for querying notes.
+	ParamsFindNotes struct {
+		Query string `json:"query,omitempty"`
+	}
+
+	// ResultFindNotes represents the return value of querying notes
+	ResultNotesInfo struct {
+		NoteId    int64                `json:"noteId,omitempty"`
+		ModelName string               `json:"modelName,omitempty"`
+		Fields    map[string]FieldData `json:"fields,omitempty"`
+		Tags      []string             `json:"tags,omitempty"`
+	}
+
+	// ParamsNotesInfo represents the ankiconnect API params for getting note info.
+	ParamsNotesInfo struct {
+		Notes *[]int64 `json:"notes,omitempty"`
 	}
 
 	// Note represents a Anki Note.
@@ -40,6 +59,12 @@ type (
 
 	// Fields represents the main fields for a Anki Note
 	Fields map[string]string
+
+	// FieldData represents the format of a field returned by ankiconnect
+	FieldData struct {
+		Value string `json:"value,omitempty"`
+		Order int64  `json:"order,omitempty"`
+	}
 
 	// Options represents note options.
 	Options struct {
@@ -82,8 +107,8 @@ type (
 
 // Add adds a new note in Anki.
 // The method returns an error if:
-//     - the api request to ankiconnect fails.
-//     - the api returns a http error.
+//   - the api request to ankiconnect fails.
+//   - the api returns a http error.
 func (nm *notesManager) Add(note Note) *errors.RestErr {
 	params := ParamsCreateNote{
 		Note: &note,
@@ -93,4 +118,24 @@ func (nm *notesManager) Add(note Note) *errors.RestErr {
 		return restErr
 	}
 	return nil
+}
+
+func (nm *notesManager) Get(query string) (*[]ResultNotesInfo, *errors.RestErr) {
+	findParams := ParamsFindNotes{
+		Query: query,
+	}
+	noteIds, restErr := post[[]int64](nm.Client, ActionFindNotes, &findParams)
+	if restErr != nil {
+		return nil, restErr
+	}
+
+	infoParams := ParamsNotesInfo{
+		Notes: noteIds,
+	}
+
+	notes, restErr := post[[]ResultNotesInfo](nm.Client, ActionNotesInfo, &infoParams)
+	if restErr != nil {
+		return nil, restErr
+	}
+	return notes, nil
 }
--- notes_test.go
+++ notes_test.go
@@ -21,7 +21,8 @@ func TestNotesManager_Add(t *testing.T) {
 		httpmock.RegisterResponder(http.MethodPost, ankiConnectUrl, responder)
 
 		note := new(Note)
-		loadTestData(t, testDataPath+ActionAddNote+"Payload"+jsonExt, result)
+		// This doesn't actually do anything
+		loadTestData(t, testDataPath+ActionAddNote+"Payload"+jsonExt, note)
 		restErr := client.Notes.Add(*note)
 		assert.Nil(t, restErr)
 	})
@@ -40,6 +41,61 @@ func TestNotesManager_Add(t *testing.T) {
 		note := new(Note)
 		loadTestData(t, testDataPath+ActionAddNote+"Payload"+jsonExt, result)
 		restErr := client.Notes.Add(*note)
+		assert.NotNil(t, restErr)
+		assert.Equal(t, http.StatusBadRequest, restErr.StatusCode)
+		assert.Equal(t, "some error message", restErr.Message)
+	})
+}
+
+func TestNotesManager_Get(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		httpmock.ActivateNonDefault(client.httpClient.GetClient())
+		defer httpmock.DeactivateAndReset()
+
+    // Get will do two api calls, first findNotes to get the note id's
+		findResult := new(Result[[]int64])
+		loadTestData(t, testDataPath+ActionFindNotes+"Result"+jsonExt, findResult)
+		findResponse, err := httpmock.NewJsonResponse(http.StatusOK, findResult)
+		assert.NoError(t, err)
+
+    // Then notesInfo to transform those into actual anki notes
+		infoResult := new(Result[[]ResultNotesInfo])
+		loadTestData(t, testDataPath+ActionNotesInfo+"Result"+jsonExt, infoResult)
+		assert.Equal(t, infoResult.Result[0].ModelName, "Basic")
+		infoResponse, err := httpmock.NewJsonResponse(http.StatusOK, infoResult)
+		assert.NoError(t, err)
+
+		responder := httpmock.ResponderFromMultipleResponses(
+			[]*http.Response{
+        findResponse, 
+        infoResponse,
+			},
+		)
+
+		httpmock.RegisterResponder(http.MethodPost, ankiConnectUrl, responder)
+
+		// If possible test to make sure the payload is properly transformed into
+		// findNotesPayload.json (which seems to be attempted in above tests but is not
+		// actually working)
+		payload := "deck:current"
+		notes, restErr := client.Notes.Get(payload)
+		assert.Nil(t, restErr)
+		assert.Equal(t, len(*notes), 1)
+
+	})
+
+	t.Run("error", func(t *testing.T) {
+		httpmock.ActivateNonDefault(client.httpClient.GetClient())
+		defer httpmock.DeactivateAndReset()
+
+		result := new(Result[string])
+		loadTestData(t, testDataPath+errorTestDataFileName, result)
+		responder, err := httpmock.NewJsonResponder(http.StatusOK, result)
+		assert.NoError(t, err)
+
+		httpmock.RegisterResponder(http.MethodPost, ankiConnectUrl, responder)
+
+    _, restErr := client.Notes.Get("deck:current")
 		assert.NotNil(t, restErr)
 		assert.Equal(t, http.StatusBadRequest, restErr.StatusCode)
 		assert.Equal(t, "some error message", restErr.Message)
